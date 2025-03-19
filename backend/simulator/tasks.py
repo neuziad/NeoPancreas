f7762e2b60069e547d5f9eeaf59631a6e2a865e3
neuziad <ziadkrekshi@gmail.com>
2025-03-19 10:34:32 +0000

    Improved style for alert modals

--- backend/simulator/tasks.py
+++ backend/simulator/tasks.py
@@ -98,9 +98,17 @@ def create_reading(*args):
             if new_reading.adjust_for_noise(reading_value) is not None
             else 0
         )
+
+        # Calculate trend data
         trend_rate = new_reading.calculate_trend()
         new_reading.trend = new_reading.detect_trend_alert(trend_rate)
-        new_reading.basal_injected = Decimal(str(user.profile.titrate_basal()))
+
+        # Generate insulin doses
+        basal_dose = user.profile.titrate_basal()
+        if user.profile.em_enabled:
+            basal_dose *= Decimal("0.75")
+            
+        new_reading.basal_injected = Decimal(str(basal_dose))
         new_reading.bolus_injected = Decimal(str(user.profile.pending_bolus))
 
         # Reset pending bolus on user profile if bolus was injected
